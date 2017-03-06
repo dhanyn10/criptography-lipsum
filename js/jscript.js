cf24bfc5b4a60b1b36b9ead1a91d79213aca4cd3
dhanyn10 <dhanyn.dhan@gmail.com>
2017-03-06 10:43:29 +0700

    comments

--- js/jscript.js
+++ js/jscript.js
@@ -1,6 +1,7 @@
 $("#generate-random-number").click(function(){
     var numval = $("#numbermax").val();
     
+    //array to store data random number
     var randomnumber = [];
     
     for(var a = 0; a < numval; a++)
@@ -13,7 +14,9 @@ $("#generate-random-number").click(function(){
             while(randomnumber[b] == random)
                 random = Math.floor((Math.random() * numval) + 1);
         }
+        //insert data random to array
         randomnumber.push(random);
     }
+    //display data inside '#result' value
     $("#result").val(randomnumber);
 });
