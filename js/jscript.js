40d7af8e4189f7c8366944d174ebb2f266ceccf2
dhanyn10 <dhanyn.dhan@gmail.com>
2017-03-06 10:38:59 +0700

    generate random number without any duplicated value

--- js/jscript.js
+++ js/jscript.js
@@ -1,5 +1,19 @@
 $("#generate-random-number").click(function(){
     var numval = $("#numbermax").val();
-    var random = Math.floor((Math.random() * numval) + 1);
-    $("#result").val(random); 
+    
+    var randomnumber = [];
+    
+    for(var a = 0; a < numval; a++)
+    {
+        //var regenerate = true;
+        random = Math.floor((Math.random() * numval) + 1);
+        for(var b = 0; b < numval; b++)
+        {
+            //check if any index of array has same value with random number
+            while(randomnumber[b] == random)
+                random = Math.floor((Math.random() * numval) + 1);
+        }
+        randomnumber.push(random);
+    }
+    $("#result").val(randomnumber);
 });
